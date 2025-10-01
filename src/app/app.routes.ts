20f8c18bbd836d58ea5dfcc9ac467b9b1715c073
Sonia CHALAL <contact@crea2code.fr>
2025-10-01 17:25:39 +0200

    feat(dashboard): page standalone + route /dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-  // Tu ajouteras ces modules plus tard quand tu les généreras :
-  // { path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard/dashboard').then(m => m.DashboardComponent)
+  },
+  { path: '**', redirectTo: 'dashboard' }
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
+import 'zone.js';  // <-- indispensable si tu n’as pas de polyfills.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
